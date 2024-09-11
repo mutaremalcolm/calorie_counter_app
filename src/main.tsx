ef74a003f5dd53dea0537d210fb7e199b5ae0fa5
“malcolmmutare” <mutarem@gmail.com>
2024-09-11 16:36:19 +0200

    project rescaffolding

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-// Import the layouts
-import RootLayout from "./layouts/root_layout";
-import DashboardLayout from "./layouts/dashboard_layout";
-
-// Import the components
-import IndexPage from "./routes";
-import ContactPage from "./routes/contact";
-import SignInPage from "./routes/signIn";
-import SignUpPage from "./routes/signUp";
-import DashboardPage from "./routes/dashboard";
-
-const router = createBrowserRouter([
-  {
-    element: <RootLayout />,
-    children: [
-      { path: "/", element: <IndexPage /> },
-      { path: "/contact", element: <ContactPage /> },
-      { path: "/sign-in/*", element: <SignInPage /> },
-      { path: "/sign-up/*", element: <SignUpPage /> },
-      {
-        element: <DashboardLayout />,
-        path: "dashboard",
-        children: [{ path: "/dashboard", element: <DashboardPage /> }],
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
