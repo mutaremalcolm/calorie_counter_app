7005c5295af7e66f83d9244e304c752d170833f9
“malcolmmutare” <mutarem@gmail.com>
2024-10-16 10:49:38 +0200

    partial implementation of firebase federated identinty provider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,3 @@
-// src/context/AuthContext.tsx
 import React, { createContext, useContext, useState, ReactNode } from "react";
 import { auth } from "../lib/firebase/firebaseConfig";
 import { signOut, onAuthStateChanged, User } from "firebase/auth";
